82ba44d5679a5264e9c72fd03625bbe46706dde7
vijaypatel35136 <vijaypatel35136@gmail.com>
2026-01-08 12:07:16 +0530

    Delete admin-panel directory

--- admin-panel/src/components/StatCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function StatCard({ title, value }) {
-  return (
-    <div className="bg-white p-6 rounded-xl shadow">
-      <p className="text-gray-500">{title}</p>
-      <h2 className="text-3xl font-bold mt-2">{value}</h2>
-    </div>
-  );
-}
